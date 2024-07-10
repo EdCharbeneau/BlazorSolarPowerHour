4d09359f65fc17ec07698e458bf6ac9fac78b270
ed.charbeneau@gmail.com <ed.charbeneau@gmail.com>
2024-07-10 12:08:19 -0400

    Added Telerik UI and updated App layout

--- BlazorSolarPowerHour/Program.cs
+++ BlazorSolarPowerHour/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddTelerikBlazor();
 builder.Services.AddScoped<MQTTUIService>();
 
 var app = builder.Build();
