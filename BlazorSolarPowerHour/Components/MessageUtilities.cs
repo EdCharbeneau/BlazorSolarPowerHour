0a10348b12f9d4b3ed5131f0cccaef80100bdf92
ed.charbeneau@gmail.com <ed.charbeneau@gmail.com>
2024-07-17 11:55:01 -0400

    Added Power messages to UI. Added helper lib for message payload.

---
+++ BlazorSolarPowerHour/Components/MessageUtilities.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+namespace BlazorSolarPowerHour.Components
+{
+    public static class MessageUtilities
+    {
+        public static string GetTopicValue(this ArraySegment<byte> bytes) => Encoding.ASCII.GetString(bytes.ToArray());
+    }
+}
